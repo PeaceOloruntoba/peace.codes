5957c7e5434fa2a686c568a0504e06105cf51191
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-12-11 19:02:38 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 export default function App() {
   return (
-    <div className="w-screen h-screen flex items-center justify-center bg-gradient-to-r from-main via-primary to-secondary">
+    <div className="w-screen h-screen flex items-center justify-center bg-main">
       <p className="text-5xl text-darkText font-bold">Hello World!</p>
     </div>
   );
