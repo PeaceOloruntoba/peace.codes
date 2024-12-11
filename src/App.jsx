fbe06a3f57b55d24fa00185f042d6278176f1a38
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-12-11 19:01:39 +0100

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
-    <div>
-      
+    <div className="w-screen h-screen flex items-center justify-center bg-gradient-to-r from-main via-primary to-secondary">
+      <p className="text-5xl text-darkText font-bold">Hello World!</p>
     </div>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: "#000000",
+        primary: "#302340 ",
+        secondary: "#1F2544 ",
+        darkText: "#6A1E55",
+        darkLink: "#474F7A "
+      },
+      height: {
+        screen: "100dvh",
+        "screen-small": "100svh",
+        "screen-large": "100lvh",
+      },
+      weight: {
+        screen: "100dvw",
+        "screen-small": "100svw",
+        "screen-large": "100lvw",
+      },
+    },
   },
   plugins: [],
-}
-
+};
