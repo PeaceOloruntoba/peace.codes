21f96f2c78cb39f99a51b0f16d1477d97078b41a
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-12-12 11:42:06 +0100

    gggg

--- src/App.jsx
+++
@@ -1,7 +0,0 @@
-export default function App() {
-  return (
-    <div className="w-screen h-screen flex items-center justify-center bg-main">
-      <p className="text-5xl text-darkText font-bold">Hello World!</p>
-    </div>
-  );
-}
--- src/component/container/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Button(className = "", value, ...props) {
-  return (
-    <button className={className} {...props}>
-      {value}
-    </button>
-  );
-}
