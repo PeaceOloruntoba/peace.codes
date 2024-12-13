0ac07d82835023695a5b216e1c1e6964cfeee82d
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-12-13 10:58:00 +0100

    Update icon-button.tsx

--- src/components/general/icon-button.tsx
+++ src/components/general/icon-button.tsx
@@ -1,19 +1,19 @@
-import * as React from 'react';
-import { cva, type VariantProps } from 'class-variance-authority';
+import * as React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
 
-import { mergeClasses } from '@/lib/utils';
+import { mergeClasses } from "@/lib/utils";
 
 const iconButtonVariants = cva(
-  'flex justify-center items-center hover:bg-gray-100 active:bg-gray-200 rounded-lg p-1.5 transition-colors duration-200 [&_svg]:stroke-gray-600 [&_svg]:hover:stroke-gray-700',
+  "flex justify-center items-center hover:bg-gray-100 active:bg-gray-200 rounded-lg p-1.5 transition-colors duration-200 [&_svg]:stroke-gray-600 [&_svg]:hover:stroke-gray-700",
   {
     variants: {
       size: {
-        md: '[&_svg]:w-6 [&_svg]:h-6',
-        lg: '[&_svg]:w-8 [&_svg]:h-8',
+        md: "[&_svg]:w-6 [&_svg]:h-6",
+        lg: "[&_svg]:w-8 [&_svg]:h-8",
       },
     },
     defaultVariants: {
-      size: 'md',
+      size: "md",
     },
   }
 );
@@ -21,7 +21,6 @@ const iconButtonVariants = cva(
 export interface IconButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof iconButtonVariants> {
-  asChild?: boolean;
   showTooltip?: boolean;
   tooltipText?: string;
 }
@@ -31,9 +30,8 @@ const IconButton = React.forwardRef<HTMLButtonElement, IconButtonProps>(
     {
       className,
       size,
-      asChild = false,
       showTooltip = false,
-      tooltipText = '',
+      tooltipText = "",
       children,
       ...props
     },
@@ -42,7 +40,7 @@ const IconButton = React.forwardRef<HTMLButtonElement, IconButtonProps>(
     return (
       <button
         className={mergeClasses(
-          'relative',
+          "relative",
           iconButtonVariants({ size }),
           className
         )}
@@ -60,6 +58,6 @@ const IconButton = React.forwardRef<HTMLButtonElement, IconButtonProps>(
   }
 );
 
-IconButton.displayName = 'IconButton';
+IconButton.displayName = "IconButton";
 
 export default IconButton;
