cf06564bcba0d086ee08bef7e8a894d3a5e877cd
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2024-12-19 08:16:32 +0100

    bns

--- src/lib/data.tsx
+++ src/lib/data.tsx
@@ -20,7 +20,7 @@ import LogoCypressLight from "../../public/images/logos/icon-cypress-light.svg";
 import LogoStorybook from "../../public/images/logos/icon-storybook.svg";
 import LogoGit from "../../public/images/logos/icon-git.svg";
 
-import LogoUpwork from "../../public/images/logos/logo-upwork.svg";
+import LogoUpwork from "../../public/images/logos/logo-bunzi.png";
 import LogoGreenApex from "../../public/images/logos/logo-bct.png";
 import LogoGreenApexLight from "../../public/images/logos/logo-bct.png";
 import LogoDotnpixel from "../../public/images/logos/logo-trusoft.jpg";
