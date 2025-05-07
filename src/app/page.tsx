0c1d702a8a38cc389ac76860920f57c901472247
Peace Oloruntoba <116691242+PeaceOloruntoba@users.noreply.github.com>
2025-05-07 15:23:22 +0100

    sss

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import AboutMeSection from '@/components/sections/about-me';
 import SkillsSection from '@/components/sections/skills';
 import ExperienceSection from '@/components/sections/experiences';
 // import TestimonialsSection from '@/components/sections/testimonials';
-// import WorkSection from '@/components/sections/work';
+import WorkSection from '@/components/sections/work';
 
 export default function Home() {
   return (
@@ -13,7 +13,7 @@ export default function Home() {
       <AboutMeSection />
       <SkillsSection />
       <ExperienceSection />
-      {/* <WorkSection /> */}
+      <WorkSection />
       {/* <TestimonialsSection /> */}
       <ContactSection />
     </>
